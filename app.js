1b5382e7c5a9a088d0ac221da6dd1fb5ad463f6e
Lee Tuan <tuanlm1989@gmail.com>
2019-03-21 09:51:26 +0700

    update

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 global.__dirbase = __dirname;
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 const apiDashoard = require('./routes/apiDashboard');
 
+app.use(express.static('public'))
 app.use('/apiBoard', apiDashoard);
+app.get('/board/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/dashboard', 'index.html'));
+});
 app.get('/', function (req, res) {
   res.send('This is express server');
 })
-
 app.listen(port, function () {
   console.log(`Server is listening on port: ${port}`);
 })
---
+++ public/dashboard/precache-manifest.dfd6dc38dfa8bdd89420b74951fde7d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "260dd14fc9444401f090",
+    "url": "/dashboard/static/css/main.85e17e65.chunk.css"
+  },
+  {
+    "revision": "260dd14fc9444401f090",
+    "url": "/dashboard/static/js/main.5e40ac30.chunk.js"
+  },
+  {
+    "revision": "fa4cf97a89d4c21ecfec",
+    "url": "/dashboard/static/js/runtime~main.4644b141.js"
+  },
+  {
+    "revision": "a19b8c056795b22cc0e7",
+    "url": "/dashboard/static/css/2.51425127.chunk.css"
+  },
+  {
+    "revision": "a19b8c056795b22cc0e7",
+    "url": "/dashboard/static/js/2.75db5800.chunk.js"
+  },
+  {
+    "revision": "e775178c7478e4d282f28b067fae2e64",
+    "url": "/dashboard/index.html"
+  }
+];
